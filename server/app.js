7f439d0f62f477f6ea903a5423843690abacea66
liaoyoujia <2578102087@qq.com>
2019-07-07 16:27:06 +0800

    first commit

---
+++ server/app.js
@@ -0,0 +1,35 @@
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
+let blogRouter = require('./routes/blog');
+let usersRouter = require('./routes/users');
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+const redisClient = require('./db/redis')
+const sessionStore = new RedisStore({
+  client: redisClient
+})
+app.use(session({
+  secret: 'WJiol#23123_',
+  cookie: {
+    // path: '/',   // 默认配置
+    // httpOnly: true,  // 默认配置
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: sessionStore
+}))
+
+app.use('/api/users', usersRouter);
+app.use('/api/blogs', blogRouter);
+
+module.exports = app;
