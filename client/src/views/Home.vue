43780a88e9517ad0aed9756f0e36044c894452d9
liaoyoujia <2578102087@qq.com>
2019-07-24 18:31:04 +0800

    修改

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,65 +1,72 @@
 <template>
   <div class="home">
-    <el-container>
-      <el-header class="el-header-block">
-        <div class="el-header-block-left">
-          <div class="header-item">
+    <el-row type="flex" class="home-header" justify="center" align="center">
+      <el-col :span="20" class="home-header-content">
+        <div class="home-header-left">
+          <div class="home-header-pane">
             <span>首页</span>
             <i class="iconfont icon-shouye1"></i>
           </div>
-          <div class="header-item">
-            <span>留言板</span>
+          <div class="home-header-pane">
+            <span>留言</span>
             <i class="iconfont icon-liuyan"></i>
           </div>
-          <div class="header-item">
+          <div class="home-header-pane">
             <span>关于</span>
             <i class="iconfont icon-iconset0142"></i>
           </div>
         </div>
-        <div class="user">
-          <span>溯夜</span>
-          <img src="../assets/logo.jpg" alt="" />
+        <div class="home-header-right">
+          <div class="home-header-pane">
+            <span>溯夜</span>
+            <i class="iconfont icon-yonghu"></i>
+          </div>
         </div>
-      </el-header>
-      <el-container class="el-swiper-block">
-        <swiper :options="swiperOption" class="swiper">
-          <swiper-slide class="swiper-item"><img src="../assets/1.jpeg"/></swiper-slide>
-          <swiper-slide class="swiper-item"><img src="../assets/2.jpeg"/></swiper-slide>
-          <swiper-slide class="swiper-item"><img src="../assets/3.jpg"/></swiper-slide>
+      </el-col>
+    </el-row>
+    <el-row type="flex" justify="center">
+      <el-col :span="20">
+        <swiper :options="swiperOption" ref="mySwiper" class="swiper">
+          <!-- slides -->
+          <swiper-slide><img src="../assets/swiper_1.jpg" alt=""></swiper-slide>
+          <swiper-slide><img src="../assets/swiper_2.jpg" alt=""></swiper-slide>
+          <!-- Optional controls -->
           <div class="swiper-pagination" slot="pagination"></div>
           <div class="swiper-button-prev" slot="button-prev"></div>
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
-      </el-container>
-      <el-container class="el-main-block">
-        <el-container>
-          <el-main>Main</el-main>
-          <el-footer>Footer</el-footer>
-        </el-container>
-        <el-aside class="el-aside-block"
-          ><div class="personal-infor">
-            <img
-              src="https://avatars2.githubusercontent.com/u/41510578?s=180&v=4"
-              alt=""
-            />
+      </el-col>
+    </el-row>
+    <el-row type="flex" justify="center" class="home-main">
+      <el-col :span="20">
+        <el-row type="flex" justify="center" >
+          <el-col :span="20">3</el-col>
+          <el-col :span="4" class="perosonal-infor">
+            <div class="avater">
+              <img
+                src="https://avatars0.githubusercontent.com/u/41510578?s=400&u=255b0c40319bc8d9d78652b71d41536f5ad72e42&v=4"
+                alt=""
+              />
+            </div>
             <h3>溯夜</h3>
-            <div class="pane-item">
-              <i class="iconfont icon-dizhi"></i>
-              <span>南京</span>
+            <div class="personal-detail">
+              <div>
+                <i class="iconfont icon-dizhi"></i>
+                <span>地址</span>
+              </div>
+              <div>
+                <i class="iconfont icon-wangzhi"></i>
+                <span>网址</span>
+              </div>
+              <div>
+                <i class="iconfont icon-22"></i>
+                <span>邮件</span>
+              </div>
             </div>
-            <div class="pane-item">
-              <i class="iconfont icon-wangzhi"></i>
-              <span>博客网址</span>
-            </div>
-            <div class="pane-item">
-              <i class="iconfont icon-22"></i>
-              <span>邮箱</span>
-            </div>
-          </div></el-aside
-        >
-      </el-container>
-      <el-footer>Footer</el-footer>
-    </el-container>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -114,99 +121,79 @@
   }
 </script>
 <style lang="scss" scope>
-  i {
-    font-size: 18px !important;
-    margin-left: 3px;
-    position: relative;
-    top: 1px;
-  }
   .home {
-    width: 100%;
-    margin: 0 auto;
-    .el-header-block {
-      width: 100%;
-      display: flex;
-      box-sizing: border-box;
-      padding: 0 150px;
-      justify-content: space-between;
-      align-items: center;
+    .home-header {
       background-color: #24292e;
-      font-size: 14px;
-      line-height: 1.5;
-      color: #fff;
-      box-shadow: 0 0px 2px #24292e;
-      .header-item {
+      height: 50px;
+      box-sizing: border-box;
+      .home-header-content {
         display: flex;
-        justify-content: flex-start;
-        /* align-items: center; */
-        align-items: baseline;
-        margin-right: 30px;
+        justify-content: space-between;
+        align-items: center;
+        color: #fff;
+        .home-header-pane {
+          margin-right: 50px;
+        }
+        .home-header-left {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+        }
+        .home-header-right {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+        }
       }
-      .el-header-block-left {
-        display: flex;
+    }
+    .swiper{
+      img{
+        width: 100%;
+        height: 300px;
       }
-      .user {
+    }
+    .home-main {
+      .perosonal-infor {
+        border: 1px solid #333;
+        background: rgba(50, 50, 50, 0.5);
+        padding:30px 0;
         display: flex;
+        flex-direction: column;
         justify-content: flex-start;
         align-items: center;
-        img {
-          width: 18px;
-          height: 18px;
+        .avater {
+          width: 60px;
+          height: 60px;
           border-radius: 50%;
-          display: block;
-          margin-left: 4px;
-        }
-      }
-    }
-    .el-swiper-block {
-      width: 100%;
-      box-sizing: border-box;
-      padding: 0 150px;
-      .swiper-item{
-        img{
-          max-width: 100%;
-          height: 400px;
-        }
-      }
-    }
-    .el-main-block {
-      width: 100%;
-      box-sizing: border-box;
-      padding: 0 150px;
-      height: 900px;
-      .el-aside-block {
-        margin-top: 30px;
-        width: 20% !important;
-        .personal-infor {
-          width: 80%;
-          margin-left: 10%;
-          margin-right: 10%;
-          display: flex;
-          flex-direction: column;
-          justify-content: flex-start;
-          align-items: flex-start;
-          border:1px solid #ccc;
+          overflow: hidden;
           img {
-            width: 100%;
+            display: block;
+            width: 60px;
+            height: 60px;
           }
-          h3 {
-            width: 100%;
-            text-align: center;
-            font-size: 26px;
-            font-weight: 700;
+        }
+        .personal-detail {
+          width: 100%;
+          display: flex;
+          justify-content: flex-start;
+          flex-direction: column;
+          align-items: flex-start;
+          box-sizing: border-box;
+          & > div {
+            margin-bottom: 10px;
+            margin-left: 20px;
           }
-          .pane-item {
-            width: 80%;
-            display: flex;
-
-            justify-content: flex-start;
-            align-items: center;
-            i{
-              margin-right: 10px;
-            }
+          i {
+            margin: 0 8px 0 0;
           }
         }
       }
+    }
+    i {
+      font-size: 18px !important;
+      margin-left: 4px;
+      position: relative;
+      top: 2px;
     }
   }
 </style>
